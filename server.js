0a5c645095a99549ef6043f20371111d03609a4a
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-11-19 20:11:32 +0100

    Working baseline

--- server.js
+++ server.js
@@ -1,53 +1,45 @@
-import { Elysia, t } from "elysia";
-import { db } from "./chdb.js";
+import { db, chdb } from "./chdb.js";
+import { Elysia } from "elysia";
 
 let PORT = process.env.PORT || 8123
 let HOST = process.env.HOST || '0.0.0.0'
 
-const conn = new db('CSV')
+const conn = new db('CSV', '/tmp')
+console.log("version:", conn.query("SELECT chdb()"));
+
+const chdb_query = function(query, format){
+    let result = conn.query(query, format);
+    return result;
+}
 
 const app = new Elysia()
         .onRequest(({ set }) => {
-		set.headers = {
-			'Access-Control-Allow-Origin': '*'
-		}
-	})
+          set.headers = { 'Access-Control-Allow-Origin': '*' }
+        })
         .onError(({ code, error }) => {
           return new Response(error.toString())
         })
-	.get('/play', () => Bun.file('public/play.html'))
+        .get('/version', () => chdb_query("SELECT version(), chdb()"))
+        .get('/play', () => Bun.file('public/play.html'))
         .get("/", ({ query }) => {
-
           if (!query.query) throw new Error('no query, no party.')
-	  if (!query.format) query.format = query.default_format || 'CSV';
-	  console.log("query", query);
-
-	  let result = conn.query(query.query, query.format);
-	  console.log("res", result);
-	  return result.toString();
-
+          if (!query.format) query.format = query.default_format || 'CSV';
+          let result = chdb_query(query.query.toString(), query.format.toString());
+          return result.toString();
         })
         .post("/", ({ body, query }) => {
-
-	  if (!query.query && body) {
-		query.query = body
-		body = false
-	  }
-	  if (query.database)
-		console.log(query.database);
-
+          if (!query.query && body) {
+                query.query = body
+                body = false
+          }
+          if (query.database)
+                chdb_query("USE "+query.database")
           if (!query.query) throw new Error('no query, no party.')
-	  if (!query.format) query.format = query.default_format || 'CSV';
-
-	  console.log("query", query);
-
-	  let result = conn.query(query.query, query.format);
-	  console.log("res", result);
-
-	  return result.toString()
-
+          if (!query.format) query.format = query.default_format || 'CSV';
+          let result = chdb_query(query.query.toString(), query.format.toString());
+          return result.toString()
         })
-	.post('/mirror', ({ body }) => body)
-	.listen({ port: PORT, hostname: HOST })
+        .post('/mirror', ({ body }) => body)
+        .listen({ port: PORT, hostname: HOST })
 
-console.log('🦊Elysia is running at :'+PORT)
+console.log('🐍 chdb-bun API running at :'+PORT)
